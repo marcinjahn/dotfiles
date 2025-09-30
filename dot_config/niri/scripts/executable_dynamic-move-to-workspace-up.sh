9d6c540dc9bbc17e9e541cc4b021f551642ab876
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2025-09-30 09:32:32 +0200

    niri: Do not move window up when not needed

--- dot_config/niri/scripts/executable_dynamic-move-to-workspace-up.sh
+++ dot_config/niri/scripts/executable_dynamic-move-to-workspace-up.sh
@@ -12,15 +12,24 @@ workspaces_state=$(niri msg --json workspaces)
 
 focused_workspace=$(echo "$workspaces_state" | jq '.[] | select(.is_focused == true)')
 focused_idx=$(echo "$focused_workspace" | jq -r '.idx')
-focused_output=$(echo "$focused_workspace" | jq -r '.output')
 
 if [ "$focused_idx" -ne 1 ]; then
   niri msg action move-window-up-or-to-workspace-up
   exit 0
-else
-  last_workspace_idx=$(echo "$workspaces_state" | jq --arg output "$focused_output" '.[] | select(.output == $output) | .idx' | sort -n | tail -n 1)
-
-  niri msg action move-window-to-workspace --focus false "$last_workspace_idx"
-  niri msg action move-workspace-to-index --reference "$last_workspace_idx" 1
-  niri msg action focus-workspace-up
 fi
+
+focused_id=$(echo "$focused_workspace" | jq -r '.id')
+
+windows_state=$(niri msg --json windows)
+window_count=$(echo "$windows_state" | jq --argjson id "$focused_id" '[.[] | select(.workspace_id == $id)] | length')
+
+if [ "$window_count" -eq 1 ]; then
+  exit 0
+fi
+
+focused_output=$(echo "$focused_workspace" | jq -r '.output')
+last_workspace_idx=$(echo "$workspaces_state" | jq --arg output "$focused_output" '.[] | select(.output == $output) | .idx' | sort -n | tail -n 1)
+
+niri msg action move-window-to-workspace --focus false "$last_workspace_idx"
+niri msg action move-workspace-to-index --reference "$last_workspace_idx" 1
+niri msg action focus-workspace-up
