d2b525c5c410902a2a2d396a4318e96921dbad14
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2025-12-18 12:53:09 +0100

    update fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,7 +3,7 @@ set -gx VISUAL nvim
 set -gx PAGER less
 set -gx LESS -eiRMX
 
-set -gx DOCKER_HOST unix:///run/user/1000/podman/podman.sock
+# set -gx DOCKER_HOST unix:///run/user/1000/podman/podman.sock
 # set -gx NVM_LAZY_LOAD true
 # set -gx NVM_COMPLETION true
 set -gx DOTNET_CLI_TELEMETRY_OPTOUT 1
@@ -11,13 +11,13 @@ set -gx DOTNET_UPGRADEASSISTANT_TELEMETRY_OPTOUT 1
 set -gx FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT 1
 set -gx DOTNET_ROOT $HOME/.dotnet
 set -gx ASDF_GOLANG_MOD_VERSION_ENABLED true
-set -gx MONO_GAC_PREFI /home/linuxbrew/.linuxbrew
+# set -gx MONO_GAC_PREFIX /home/linuxbrew/.linuxbrew
 
 set -U fish_greeting
 # set --universal nvm_default_version v18
 
 fish_add_path ~/bin
-fish_add_path ~/bin/distrobox
+# fish_add_path ~/bin/distrobox
 fish_add_path ~/sbin
 fish_add_path ~/scripts
 fish_add_path ~/opt/homebrew/bin
@@ -30,6 +30,8 @@ fish_add_path ~/.dotnet
 fish_add_path ~/.dotnet/tools
 fish_add_path ~/.cargo/bin
 fish_add_path ~/bin/netcoredbg
+fish_add_path ~/bin/vicinae/bin
+fish_add_path ~/.npm-global/bin
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
@@ -50,6 +52,7 @@ set --erase _asdf_shims
 
 if status is-interactive
     alias v="nvim"
+    alias ghostty="/home/mnj/AppImages/ghostty.appimage"
     abbr --add wifilist nmcli device wifi list
     abbr --add wificonnect nmcli device wifi connect
 end
