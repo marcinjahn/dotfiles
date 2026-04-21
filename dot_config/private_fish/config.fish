dd268ef917c841024c38881771475289bd9ef616
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2026-04-21 19:42:11 +0200

    Update fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -11,10 +11,28 @@ set -gx DOTNET_UPGRADEASSISTANT_TELEMETRY_OPTOUT 1
 set -gx FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT 1
 set -gx DOTNET_ROOT $HOME/.dotnet
 set -gx ASDF_GOLANG_MOD_VERSION_ENABLED true
+set -gx ASDF_NODEJS_LEGACY_FILE_DYNAMIC_STRATEGY latest_installed
+set -gx OPENCODE_DISABLE_CLAUDE_CODE_SKILLS true
+set -gx OPENCODE_EXPERIMENTAL_LSP_TOOL true
 # set -gx MONO_GAC_PREFIX /home/linuxbrew/.linuxbrew
 
 set -U fish_greeting
 # set --universal nvm_default_version v18
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Always ensure shims are first, removing any existing entry first
+set -gx PATH (string match -v -- $_asdf_shims $PATH)
+set -gx --prepend PATH $_asdf_shims
+set --erase _asdf_shims
+# END OF ASDF
 
 fish_add_path ~/bin
 # fish_add_path ~/bin/distrobox
@@ -32,23 +50,6 @@ fish_add_path ~/.cargo/bin
 fish_add_path ~/bin/netcoredbg
 fish_add_path ~/bin/vicinae/bin
 fish_add_path ~/.npm-global/bin
-
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-# ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-# Do not use fish_add_path (added in Fish 3.2) because it
-# potentially changes the order of items in PATH
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
-# END OF ASDF
 
 if status is-interactive
     alias v="nvim"
